9a0fbadb006241faa3c8d1db47c29944c08b7b0e
JoeProgram <joseph.france@gmail.com>
2017-10-30 20:10:53 -0700

    Input and Blocking
    - blocked out basic scene
    - setup input working on both controllers separately

--- Assets/Scripts/MusicButton.cs
+++ Assets/Scripts/MusicButton.cs
@@ -7,28 +7,47 @@ public class MusicButton : MonoBehaviour {
 	public GameObject perimeter;
 	public GameObject face;
 
+	protected bool shouldPress;
+
+	protected Vector3 faceOutPosition;
+	protected static Vector3 faceInPosition = new Vector3(0,0,-0.036f);
+
 	protected Material perimeterMaterialDontPress;
 	public Material perimeterMaterialDoPress;
 
-	public string mAxis;
+	public string buttonName;
 
 	// Use this for initialization
 	void Start () {
 	
+		// grab default values that we'll need to restore later
 		perimeterMaterialDontPress = perimeter.GetComponent<Renderer>().material;
+		faceOutPosition = face.transform.localPosition;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetButtonDown("Top")) {
-			Debug.Log("Top Pressed");
-		} else if(Input.GetButtonDown("Middle")) {
-			Debug.Log("Middle Pressed");
-		} else if(Input.GetButtonDown("Bottom")) {
-			Debug.Log("Bottom Pressed");
+		if(Input.GetButtonDown(buttonName)) {
+			SetPressed(true);	
+		} else if(Input.GetButtonUp(buttonName)) {
+			SetPressed(false);
 		}
+
+	}
+
+	protected void SetPressed( bool pressed = true ){
+		if(pressed) {
+			face.transform.localPosition = faceInPosition;
+		} else {
+			face.transform.localPosition = faceOutPosition;
+		}
+	}
+
+	public void SetShouldPress( bool shouldPress = true ){
+		this.shouldPress = shouldPress;
+
 
 	}
 }
